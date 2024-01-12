80d4c9a2a2840b1e3821d2797a4e3788195f8ddc
Harsh Deep <deepharsh057@gmail.com>
2024-01-12 22:51:42 +0530

    Components folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import reactImg from './assets/react-core-concepts.png'
+import Header from './components/Header'
+import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data';
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} Fundamental React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-const CoreConcept = (props) => {
-  return (
-    <li>
-      <img src={props.image} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
 
 function App() {
-  const coreConcept=CORE_CONCEPTS.map((obj)=>{
+  const coreConcepts=CORE_CONCEPTS.map((obj)=>{
     return (<CoreConcept {...obj}/>)
     })
   return (
@@ -40,7 +14,7 @@ function App() {
         <h2>Time to get started!</h2>
         <section id="core-concepts">
         <ul>
-          {coreConcept}
+          {coreConcepts}
         </ul>
         </section>
       </main>
